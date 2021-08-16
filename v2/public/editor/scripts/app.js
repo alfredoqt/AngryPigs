b319f176ced8383ea21af74a90ecb7de99a8165c
Alfredo Quintero Tlacuilo <alfredoq@lyft.com>
2021-08-15 21:09:50 -0700

    Refactor on level data model

--- v2/public/editor/scripts/app.js
+++ v2/public/editor/scripts/app.js
@@ -4,32 +4,21 @@ const OBJECT_TYPES = {
     BOX: 'box',
 };
 
-// TODO: Abstract this away to a Level class
+const DATA_LEVEL_OBJECT_ID = 'data-level-object-id';
+const DATA_LEVEL_OBJECT_TYPE = 'data-level-object-type';
+
+// TODO: Abstract this away to a level reducer
 const levelIdsToElementMap = new Map();
-// {cannon: Cannon, birds: Bird[], boxes: Box[]}
-let levelObjectsData = {cannon: null, birds: {}, boxes: {}};
+// {entities: {birds: {byId: Map<ID, Bird>, ids: ID[], structure: {byId: Map<ID, Box>, ids: ID[]}}}, name: string, cannon: Cannon}
+let levelData = {entities: {birds: {byId: {}, ids: []}, structure: {byId: {}, ids: []}}, name: null, cannon: null};
 
-function onDragStartObjectArea(event) {
+function onDragStart(event) {
     const dragInitialRect = event.target.getBoundingClientRect();
     const mouseInInitialDragX = event.clientX - dragInitialRect.x;
     const mouseInInitialDragY = event.clientY - dragInitialRect.y;
-    const type = event.target.getAttribute('data-level-object-type');
+    const type = event.target.getAttribute(DATA_LEVEL_OBJECT_TYPE);
     event.dataTransfer.setData('application/json', JSON.stringify({
-        type,
-        initialDragOffset: {
-            x: mouseInInitialDragX,
-            y: mouseInInitialDragY,
-        },
-    }));
-}
-
-function onDragStartLevelArea(event) {
-    const dragInitialRect = event.target.getBoundingClientRect();
-    const mouseInInitialDragX = event.clientX - dragInitialRect.x;
-    const mouseInInitialDragY = event.clientY - dragInitialRect.y;
-    const type = event.target.getAttribute('data-level-object-type');
-    event.dataTransfer.setData('application/json', JSON.stringify({
-        id: event.target.getAttribute('data-object-id'),
+        id: event.target.getAttribute(DATA_LEVEL_OBJECT_ID),
         type,
         initialDragOffset: {
             x: mouseInInitialDragX,
@@ -43,77 +32,125 @@ function onDragOver(event) {
     event.dataTransfer.dropEffect = 'move';
 }
 
+function getDropCoordinates(mouseEvent, dropArea, objectOffsetToMouse) {
+    const dropRect = dropArea.getBoundingClientRect();
+    const mouseInDropX = mouseEvent.clientX - dropRect.x - objectOffsetToMouse.x;
+    const mouseInDropY = mouseEvent.clientY - dropRect.y - objectOffsetToMouse.y;
+    return {
+        x: mouseInDropX,
+        y: mouseInDropY,
+    };
+}
+
 function onDropNewObject(event, data) {
-    if (data.type === OBJECT_TYPES.CANNON && levelObjectsData.cannon != null) {
+    if (data.type === OBJECT_TYPES.CANNON && levelData.cannon != null) {
         return;
     }
 
     const id = uuidv4();
+
+    const dropCoordinates = getDropCoordinates(event, event.target, data.initialDragOffset);
+
+    // Update view
     const newLevelObject = document.createElement('div');
-    newLevelObject.setAttribute('data-object-id', id);
     newLevelObject.classList.add('level-object');
+    newLevelObject.setAttribute(DATA_LEVEL_OBJECT_ID, id);
+    newLevelObject.setAttribute(DATA_LEVEL_OBJECT_TYPE, data.type);
     newLevelObject.setAttribute('draggable', 'true');
-    const dropRect = event.target.getBoundingClientRect();
-    const mouseInDropX = event.clientX - dropRect.x - data.initialDragOffset.x;
-    const mouseInDropY = event.clientY - dropRect.y - data.initialDragOffset.y;
-    newLevelObject.style.left = `${mouseInDropX}px`;
-    newLevelObject.style.top = `${mouseInDropY}px`;
-    const newObject = {id, x: mouseInDropX, y: mouseInDropY};
+    newLevelObject.addEventListener('dragstart', onDragStart);
+    newLevelObject.style.left = `${dropCoordinates.x}px`;
+    newLevelObject.style.top = `${dropCoordinates.y}px`;
+    newLevelObject.classList.add(`level-object-${data.type}`);
+
+    levelIdsToElementMap.set(id, newLevelObject);
+    event.target.append(newLevelObject);
+
+    // Update data
+    const newObject = {id, x: dropCoordinates.x, y: dropCoordinates.y, type: data.type};
     switch (data.type) {
         case OBJECT_TYPES.BIRD:
-            levelObjectsData = {...levelObjectsData, birds: {
-                ...levelObjectsData.birds,
-                [newObject.id]: newObject,
-            }};
-            newLevelObject.setAttribute('data-level-object-type', OBJECT_TYPES.BIRD);
-            newLevelObject.classList.add('level-object-bird');
+            levelData = {
+                ...levelData,
+                entities: {
+                    ...levelData.entities,
+                    birds: {
+                        byId: {
+                            ...levelData.entities.birds.byId,
+                            [id]: newObject,
+                        },
+                        ids: [...levelData.entities.birds.ids, id],
+                    },
+                },
+            };
             break;
         case OBJECT_TYPES.CANNON:
-            levelObjectsData = {...levelObjectsData, cannon: newObject};
-            newLevelObject.setAttribute('data-level-object-type', OBJECT_TYPES.CANNON);
-            newLevelObject.classList.add('level-object-cannon');
+            levelData = {...levelData, cannon: newObject};
             break;
         case OBJECT_TYPES.BOX:
-            levelObjectsData = {...levelObjectsData, boxes: {
-                ...levelObjectsData.boxes,
-                [newObject.id]: newObject,
-            }};
-            newLevelObject.setAttribute('data-level-object-type', OBJECT_TYPES.BOX);
-            newLevelObject.classList.add('level-object-box');
+            levelData = {
+                ...levelData,
+                entities: {
+                    ...levelData.entities,
+                    structure: {
+                        byId: {
+                            ...levelData.entities.structure.byId,
+                            [id]: newObject,
+                        },
+                        ids: [...levelData.entities.structure.ids, id],
+                    },
+                },
+            };
             break;
         default:
             throw new Error('Level object type does not exist');
     }
-    newLevelObject.addEventListener('dragstart', onDragStartLevelArea);
-    levelIdsToElementMap.set(id, newLevelObject);
-    event.target.append(newLevelObject);
 }
 
 function onDropExistingObject(event, data) {
     const levelObject = levelIdsToElementMap.get(data.id);
 
     if (levelObject != null) {
-        const dropRect = event.target.getBoundingClientRect();
-        const mouseInDropX = event.clientX - dropRect.x - data.initialDragOffset.x;
-        const mouseInDropY = event.clientY - dropRect.y - data.initialDragOffset.y;
-        levelObject.style.left = `${mouseInDropX}px`;
-        levelObject.style.top = `${mouseInDropY}px`;
-        const updatedObject = {id: data.id, x: mouseInDropX, y: mouseInDropY};
+        const dropCoordinates = getDropCoordinates(event, event.target, data.initialDragOffset);
+        
+        // Update view
+        levelObject.style.left = `${dropCoordinates.x}px`;
+        levelObject.style.top = `${dropCoordinates.y}px`;
+
+        // Update data
+        const updatedObject = {x: dropCoordinates.x, y: dropCoordinates.y};
         switch (data.type) {
             case OBJECT_TYPES.BIRD:
-                levelObjectsData = {...levelObjectsData, birds: {
-                    ...levelObjectsData.birds,
-                    [updatedObject.id]: updatedObject,
-                }};
+                levelData = {
+                    ...levelData,
+                    entities: {
+                        ...levelData.entities,
+                        birds: {
+                            ...levelData.entities.birds,
+                            byId: {
+                                ...levelData.entities.birds.byId,
+                                [data.id]: {...levelData.entities.birds.byId[data.id], ...updatedObject},
+                            },
+                        },
+                    },
+                };
                 break;
             case OBJECT_TYPES.CANNON:
-                levelObjectsData = {...levelObjectsData, cannon: updatedObject};
+                levelData = {...levelData, cannon: {...levelData.cannon, updatedObject}};
                 break;
             case OBJECT_TYPES.BOX:
-                levelObjectsData = {...levelObjectsData, boxes: {
-                    ...levelObjectsData.boxes,
-                    [updatedObject.id]: updatedObject,
-                }};
+                levelData = {
+                    ...levelData,
+                    entities: {
+                        ...levelData.entities,
+                        structure: {
+                            ...levelData.entities.structure,
+                            byId: {
+                                ...levelData.entities.structure.byId,
+                                [data.id]: {...levelData.entities.structure.byId[data.id], ...updatedObject},
+                            },
+                        },
+                    },
+                };
                 break;
             default:
                 throw new Error('Level object type does not exist');
@@ -144,10 +181,85 @@ window.addEventListener('DOMContentLoaded', () => {
     const levelCanvas = document.querySelector('.level-canvas');
 
     objectDefaults.forEach((objectDefault) => {
-        objectDefault.addEventListener('dragstart', onDragStartObjectArea);
+        objectDefault.addEventListener('dragstart', onDragStart);
     });
 
     // Drop zone
     levelCanvas.addEventListener('drop', onDrop);
     levelCanvas.addEventListener('dragover', onDragOver);
 });
+
+// Ship {
+//     totalHealth: 5,4,3,2
+//     healthLeft: 5,4,3,2
+//     hitSpots = [false, false, false, false, false]
+//     direction: left | top | right | bottom
+//     head: [i, j]
+
+//     hit(i, j): boolean { // Boolean
+//         // Check hit spots
+//         // If hit, decrease health
+//     }
+
+
+//     isSunk() {
+//         return healthLeft === 0;
+//     }
+// }
+
+// Square {
+//     state: HIT, OCEAN, SUNK, NOT_HIT
+//     ship?: Ship (optional): Ship | undefined
+//     i: int
+//     j: int
+//     hit() {
+//         if (ship) {
+//             const isHit = ship.hit(i, j);
+//             if (isHit) {
+//                 // ChangeState
+//             }
+//         }
+//     }
+
+//     shipSunk() {
+//         return ship.isSunk()
+//     }
+// }
+
+// FleetMap { // squares: [100][100]
+
+//     // Interacts with the html
+//     hit(squareElement) {
+//         const i = squareElement.getAttribute('data-i');
+//         const j = squareElement.getAttribute('data-j');
+//         const square = squares[i][j];
+//         if (square.ship) {
+//             if (square.shipSunk()) {
+//                 return;
+//             }
+//             square.hit();
+//             if (square.shipSunk()) {
+//                 // Update all the other squares that have that ship
+//                 const head = square.ship.head;
+//                 const direction = square.direction;
+//                 if (direction === left) {
+//                 }
+//             }
+//         } else {
+//             // Is water
+//         }
+//     }
+// }
+
+// [
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+//     [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+//     [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+//     [1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+//     [1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 1, 1, 1, 1, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+// ]
